d68acc511734d331773212cba1673bf09f5dc75e
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-04-13 14:07:58 +0200

    Fix webhook

--- generate-sphinx-doc.sh
+++ generate-sphinx-doc.sh
@@ -6,6 +6,6 @@ if ! git config remote.origin.url &> /dev/null; then
   git remote add origin $1
 fi
 git fetch
-git checkout -f $2
+git checkout -f master
 
 cd infra-doc && make clean-html && make html
