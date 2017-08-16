1bbcae44293924795fe01b8a5251dedb0975a605
Marc Fournier <marc.fournier@camptocamp.com>
2017-08-16 15:00:48 +0100

    Fix hook script so it supports non-fastforward ops

--- generate-sphinx-doc.sh
+++ generate-sphinx-doc.sh
@@ -5,8 +5,7 @@ cd /var/lib/git/sphinx-doc
 if ! git config remote.origin.url &> /dev/null; then
   git remote add origin $1
 fi
-git fetch
-git checkout -f master
-git pull
+git remote update
+git checkout -f origin/master
 
 cd infra-doc && make clean-html && make html
